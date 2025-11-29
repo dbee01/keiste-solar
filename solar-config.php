19f195795fd1b73c8bc64749da137534fd62bc08
robbin hood <61830799+dbee01@users.noreply.github.com>
2025-11-29 21:34:51 +0000

    Delete solar-config.php

--- solar-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// API keys are now managed through WordPress admin settings
-// Use ksrad_get_option('google_solar_api_key') to retrieve the Solar API key
-// Use ksrad_get_option('google_maps_api_key') to retrieve the Maps API key
-// Use ksrad_get_option('report_key') to retrieve the Report API key
-
-// API Configuration
-define('GOOGLE_SOLAR_API_URL', 'https://solar.googleapis.com/v1/buildingInsights:findClosest');
